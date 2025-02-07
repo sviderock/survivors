ac6b344006d4366846100338dbe834598dbd7a33
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-02-07 12:15:33 +0200

    set server preset to vercel

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+  server: {
+    preset: 'vercel'
+  },
   vite: {
     ssr: { external: ["drizzle-orm"] },
   },
