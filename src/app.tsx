fc0eb377549f182d2faa856b985028fef1b534f9
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-02-09 23:11:43 +0200

    add more popping fonts

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,24 @@
 import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { Suspense } from 'solid-js';
-import '@fontsource/poppins';
+import '@fontsource/poppins/100.css';
+import '@fontsource/poppins/200.css';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/800.css';
+import '@fontsource/poppins/900.css';
+import '@fontsource/poppins/100-italic.css';
+import '@fontsource/poppins/200-italic.css';
+import '@fontsource/poppins/300-italic.css';
+import '@fontsource/poppins/400-italic.css';
+import '@fontsource/poppins/500-italic.css';
+import '@fontsource/poppins/600-italic.css';
+import '@fontsource/poppins/700-italic.css';
+import '@fontsource/poppins/800-italic.css';
+import '@fontsource/poppins/900-italic.css';
 import './app.css';
 
 export default function App() {
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -169,7 +169,7 @@ export default function Game() {
 
 function UIStats() {
 	return (
-		<div class="absolute right-4 top-2 flex w-[270px] flex-col items-end justify-between rounded-md border-2 bg-white p-2 px-4">
+		<div class="absolute right-4 top-2 flex w-[300px] flex-col items-end justify-between rounded-md border-2 bg-white p-2 px-4">
 			<MemoryUsage />
 			<div class="flex gap-2">
 				<Ping />
