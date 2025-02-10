af8712093dbf9d704537ab806fc8296270095a90
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-02-10 20:42:55 +0200

    add refresh to restart explanation

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -412,13 +412,14 @@ function Banner() {
 	return (
 		<div
 			class={cn(
-				'absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center text-9xl uppercase',
+				'absolute left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center text-9xl uppercase',
 				gameState.status !== 'won' && gameState.status !== 'lost' && 'hidden',
 				gameState.status === 'won' && 'bg-green-500/50',
 				gameState.status === 'lost' && 'bg-red-500/50'
 			)}
 		>
-			{gameState.status}
+			<span>{gameState.status}</span>
+			<span class="text-xl">Refresh to restart</span>
 		</div>
 	);
 }
