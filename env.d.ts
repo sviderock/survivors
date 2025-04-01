34852f8261de99378589da61957d963d133aa902
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-04-01 13:49:34 +0300

    cleanup env after appkit removal

--- env.d.ts
+++ env.d.ts
@@ -1,27 +1,21 @@
 /// <reference types="vinxi/types/client" />
 
 interface ImportMetaEnv {
-	SESSION_SECRET: string;
-	DB_URL: string;
-	VITE_REOWN_PROJECT_ID: string;
-	VITE_ZERION_API_URL: string;
-	VITE_ZERION_API_KEY: string;
-	REFERRER_ID: string;
-	REFERRER_ADDRESS: string;
+  SESSION_SECRET: string;
+  DB_URL: string;
+  VITE_ZERION_API_URL: string;
+  VITE_ZERION_API_KEY: string;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
 
 declare namespace NodeJS {
-	interface ProcessEnv {
-		readonly SESSION_SECRET: string;
-		readonly DB_URL: string;
-		readonly VITE_REOWN_PROJECT_ID: string;
-		readonly VITE_ZERION_API_URL: string;
-		readonly VITE_ZERION_API_KEY: string;
-		readonly REFERRER_ID: string;
-		readonly REFERRER_ADDRESS: string;
-	}
+  interface ProcessEnv {
+    readonly SESSION_SECRET: string;
+    readonly DB_URL: string;
+    readonly VITE_ZERION_API_URL: string;
+    readonly VITE_ZERION_API_KEY: string;
+  }
 }
